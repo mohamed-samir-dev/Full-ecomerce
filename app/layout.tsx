43ad2baa6bf8545926974403ebd58bc155b83f9f
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-01-29 13:00:48 +0200

    chore(public): add public assets folder with images and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Navbar} from "./navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,10 +24,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
+        <Navbar />
         {children}
       </body>
     </html>

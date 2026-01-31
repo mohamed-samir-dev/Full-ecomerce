5c69e3f5ff44b216e83e135d5bf3047aedb5a643
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-01-31 14:38:25 +0200

    Add TypeScript interfaces for CustomerSay, FeaturedProductCard, HeroSection, PromoSection

---
+++ app/pages/home/types/home.types.ts
@@ -0,0 +1,93 @@
+// CustomerSay
+export interface Testimonial {
+  _id: string;
+  name: string;
+  email: string;
+  rating: number;
+  comment: string;
+  isApproved: boolean;
+  createdAt: string;
+}
+export interface ReviewFormModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+  formData: FormData;
+  setFormData: (data: FormData) => void;
+  onSubmit: (e: React.FormEvent) => void;
+  isSubmitting: boolean;
+}
+
+export interface FormData {
+  name: string;
+  email: string;
+  rating: number;
+  comment: string;
+}
+// FeaturedProductCard
+export interface FeaturedProductCardProps {
+  product: {
+    _id: string;
+    name: string;
+    finalPrice: number;
+    mainImage: string;
+  };
+}
+export interface Product {
+  _id: string;
+  name: string;
+  finalPrice: number;
+  mainImage: string;
+}
+// HeroSection
+export interface HeroSlide {
+  titleKey: string;
+  descriptionKey: string;
+  image: string;
+  buttonTextKey: string;
+  buttonLink: string;
+}
+
+export interface BackgroundImageProps {
+  slide: HeroSlide;
+  currentSlide: number;
+  altText: string;
+}
+export interface HeroContentProps {
+  slide: HeroSlide;
+  currentSlide: number;
+  isDarkMode: boolean;
+  isArabic: boolean;
+  t: (key: string) => string | React.ReactNode;
+}
+export interface HeroImageProps {
+  slide: HeroSlide;
+  currentSlide: number;
+  altText: string;
+}
+export interface NavigationIndicatorProps {
+  totalSlides: number;
+  currentSlide: number;
+  onSlideChange: (index: number) => void;
+}
+
+
+
+export interface HeroSectionProps {
+  slides: HeroSlide[];
+}
+// PromoSection
+export interface PromoItem {
+  id: number;
+  title: string;
+  description: string;
+  buttonText: string;
+  imageUrl: string;
+  imageAlt: string;
+}
+
+export interface CategoryItem {
+  id: number;
+  name: string;
+  icon: string;
+  color: string;
+}

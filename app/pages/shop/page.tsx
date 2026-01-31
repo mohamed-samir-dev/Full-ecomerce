47b0b63ca00b1884c4b41b099d4f9b14d3da0be9
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-01-31 14:45:19 +0200

    feat: add shop page component

---
+++ app/pages/shop/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useFilters } from "./hooks/useFilters";
+import {
+  PageHeader,
+  FilterSidebar,
+  SortControls,
+  ProductsSection,
+} from "./components";
+
+export default function ShopPage() {
+  const {
+    filters,
+    showMobileFilters,
+    showCategoryDropdown,
+    setShowMobileFilters,
+    setShowCategoryDropdown,
+    handleFilterChange,
+    handleArrayFilterChange,
+    clearAllFilters,
+  } = useFilters();
+
+  return (
+    <div className="min-h-screen bg-white">
+      <PageHeader />
+
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 sm:py-6 lg:py-8">
+        <div className="flex flex-col lg:flex-row gap-4 sm:gap-6 lg:gap-8">
+          <FilterSidebar
+            filters={filters}
+            showMobileFilters={showMobileFilters}
+            showCategoryDropdown={showCategoryDropdown}
+            setShowMobileFilters={setShowMobileFilters}
+            setShowCategoryDropdown={setShowCategoryDropdown}
+            handleFilterChange={handleFilterChange}
+            handleArrayFilterChange={handleArrayFilterChange}
+            clearAllFilters={clearAllFilters}
+          />
+          <div className="flex-1 min-w-0">
+            <SortControls filters={filters} handleFilterChange={handleFilterChange} />
+            <ProductsSection filters={filters} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}

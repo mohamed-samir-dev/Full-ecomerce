35e04764e4cce54f6f851fa9cb064493070389ef
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-01-31 14:38:49 +0200

    Add main home page sections: Promo, Category, Featured Products, and Customer Say

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
 import HeroSection from './components/HeroSection/index';
+import CategorySection from './components/CategorySection/index';
 import PromoSection from './components/PromoSection/index';
+import FeaturedProducts from './components/FeaturedProducts/index';
+import CustomerSaySection from './components/CustomerSay/index';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { TranslationProvider } from '@/i18n';
-import {heroSlides}from  '../data/heroSlides'
+import {heroSlides}from  './data/homeData'
 
 export default function HomePage() {
   return (
@@ -13,6 +16,9 @@ export default function HomePage() {
         <main>
           <HeroSection slides={heroSlides} />
           <PromoSection />
+          <CategorySection />
+          <FeaturedProducts />
+          <CustomerSaySection />
         </main>
       </TranslationProvider>
     </ThemeProvider>

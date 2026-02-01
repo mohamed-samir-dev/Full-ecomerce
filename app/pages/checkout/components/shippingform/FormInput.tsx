64f528ef2ed4114152b86137a97a247704686a63
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-01 22:06:02 +0200

    feat: add form input component

---
+++ app/pages/checkout/components/shippingform/FormInput.tsx
@@ -0,0 +1,30 @@
+
+import {FormInputProps}from '../../types/checkout'
+export default function FormInput({ 
+  label, 
+  type = 'text', 
+  required = false, 
+  value, 
+  onChange, 
+  placeholder, 
+  icon,
+  helperText 
+}: FormInputProps) {
+  return (
+    <div>
+      <label className="block text-sm font-semibold mb-2 flex items-center text-gray-700">
+        {icon}
+        {label} {required && '*'}
+      </label>
+      <input
+        type={type}
+        required={required}
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+        placeholder={placeholder}
+        className="w-full px-3 sm:px-4 py-2.5 sm:py-3 text-sm sm:text-base border-2 rounded-xl focus:outline-none focus:border-blue-500 transition-colors bg-white border-gray-200 text-black"
+      />
+      {helperText && <p className="text-xs mt-2 text-gray-500">{helperText}</p>}
+    </div>
+  );
+}

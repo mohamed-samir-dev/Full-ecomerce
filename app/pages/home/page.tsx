e26203454b7155ca07cdcac4e30987f76bdb4d9d
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-03 19:20:57 +0200

    Update home page

--- app/pages/home/page.tsx
+++ app/pages/home/page.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import HeroSection from './components/HeroSection/index';
-import CategorySection from './components/CategorySection/index';
 import PromoSection from './components/PromoSection/index';
 import FeaturedProducts from './components/FeaturedProducts/index';
 import CustomerSaySection from './components/CustomerSay/index';
 import ClothesHangerSection from './components/ClothesHangerSection/index';
+import LuxuryCategorySection from './components/LuxuryCategorySection/index';
+import LuxuryPromoSection from './components/LuxuryPromoSection/index';
+import NewsletterSection from './components/NewsletterSection/index';
 import { ThemeProvider } from '@/context/ThemeContext';
 import { TranslationProvider } from '@/i18n';
 import {heroSlides}from  './data/homeData'
@@ -17,10 +19,12 @@ export default function HomePage() {
         <main>
           <HeroSection slides={heroSlides} />
           <PromoSection />
+          <LuxuryPromoSection />
           <ClothesHangerSection />
-          <CategorySection />
+          <LuxuryCategorySection />
           <FeaturedProducts />
           <CustomerSaySection />
+          <NewsletterSection />
         </main>
       </TranslationProvider>
     </ThemeProvider>

839b08a484b85226cabe407feb00932600868623
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-09 23:53:28 +0200

    Update about page

--- app/pages/about/page.tsx
+++ app/pages/about/page.tsx
@@ -1,7 +1,20 @@
 'use client';
 
 import About from './components/About';
+import DynamicMetadata from '@/app/components/DynamicMetadata';
 
 export default function AboutPage() {
-  return <About />;
+  return (
+    <>
+      <DynamicMetadata
+        titleAr="من نحن - عن Global Shop"
+        titleEn="About Us - About Global Shop"
+        descriptionAr="تعرف على Global Shop، متجرك الإلكتروني الموثوق للتسوق أونلاين. نوفر أفضل المنتجات بأسعار تنافسية وخدمة عملاء مميزة"
+        descriptionEn="Learn about Global Shop, your trusted online shopping destination. We offer the best products at competitive prices with excellent customer service"
+        keywordsAr={['من نحن', 'عن الشركة', 'Global Shop', 'متجر إلكتروني']}
+        keywordsEn={['about us', 'about company', 'Global Shop', 'ecommerce store']}
+      />
+      <About />
+    </>
+  );
 }

7d442f24fd23d01d62d394aeb9ce40c68fe992f3
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-10 00:08:22 +0200

    Update Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,29 @@ const nextConfig: NextConfig = {
       transform: 'react-icons/{{member}}',
     },
   },
+  headers: async () => [
+    {
+      source: '/:path*',
+      headers: [
+        {
+          key: 'X-DNS-Prefetch-Control',
+          value: 'on'
+        },
+        {
+          key: 'X-Frame-Options',
+          value: 'SAMEORIGIN'
+        },
+        {
+          key: 'X-Content-Type-Options',
+          value: 'nosniff'
+        },
+        {
+          key: 'Referrer-Policy',
+          value: 'origin-when-cross-origin'
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;

998c53807a9ae62395b7a40204625df2093ff1c7
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-09 14:34:03 +0200

    Update FormSelect component

--- app/pages/checkout/components/shippingform/FormSelect.tsx
+++ app/pages/checkout/components/shippingform/FormSelect.tsx
@@ -6,20 +6,21 @@ export default function FormSelect({
   value, 
   onChange, 
   options, 
-  placeholder = 'Select an option' 
+  placeholder = 'Select an option',
+  isDarkMode
 }: FormSelectProps) {
   const isObjectArray = options.length > 0 && typeof options[0] === 'object';
   
   return (
     <div>
-      <label className="block text-sm font-semibold mb-2 text-gray-700">
+      <label className={`block text-sm font-semibold mb-2 ${isDarkMode ? 'text-slate-300' : 'text-gray-700'}`}>
         {label} {required && '*'}
       </label>
       <select
         required={required}
         value={value}
         onChange={(e) => onChange(e.target.value)}
-        className="w-full px-3 sm:px-4 py-2.5 sm:py-3 text-sm sm:text-base border-2 rounded-xl focus:outline-none focus:border-blue-500 transition-colors bg-white border-gray-200 text-black"
+        className={`w-full px-3 sm:px-4 py-2.5 sm:py-3 text-sm sm:text-base border-2 rounded-xl focus:outline-none focus:border-blue-500 transition-colors ${isDarkMode ? 'bg-[#191C21] border-slate-600 text-white' : 'bg-white border-gray-200 text-black'}`}
       >
         <option value="">{placeholder}</option>
         {isObjectArray 

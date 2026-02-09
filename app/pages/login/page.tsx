67b98c9c4effca3bb9080da124477403980dfb23
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-10 00:01:40 +0200

    Update login page

--- app/pages/login/page.tsx
+++ app/pages/login/page.tsx
@@ -1,7 +1,20 @@
 'use client';
 
+import DynamicMetadata from '@/app/components/DynamicMetadata';
 import LoginContainer from './components/LoginContainer';
 
 export default function LoginPage() {
-  return <LoginContainer />;
+  return (
+    <>
+      <DynamicMetadata
+        titleAr="تسجيل الدخول - ادخل إلى حسابك"
+        titleEn="Login - Access Your Account"
+        descriptionAr="سجل الدخول إلى حسابك في Global Shop للوصول إلى طلباتك وقائمة الأمنيات"
+        descriptionEn="Login to your Global Shop account to access your orders and wishlist"
+        keywordsAr={['تسجيل الدخول', 'حساب', 'دخول']}
+        keywordsEn={['login', 'account', 'sign in']}
+      />
+      <LoginContainer />
+    </>
+  );
 }

257beb8a484f81f53f24ad2d517a8c9ea9b3a4f5
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-12 00:51:51 +0200

    Add orders module index for exports

---
+++ app/admin/page/orders/index.ts
@@ -0,0 +1,14 @@
+// Types
+export * from './types';
+
+// Constants
+export * from './constants';
+
+// Hooks
+export { useOrders } from './hooks/useOrders';
+
+// Utils
+export * from './utils/orderHelpers';
+
+// Components
+export * from './components';

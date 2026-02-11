38ad36b15e6ed7223f9d415b0b57f4e7ca10c6ad
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-12 00:36:06 +0200

    Add contacts components index for exports

---
+++ app/admin/page/contacts/components/index.ts
@@ -0,0 +1,5 @@
+export { StatsCards } from './StatsCards';
+export { SearchFilter } from './SearchFilter';
+export { LoadingSpinner } from './LoadingSpinner';
+export { ContactCard } from './ContactCard';
+export { ContactModal } from './ContactModal';

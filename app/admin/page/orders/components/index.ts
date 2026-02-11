89558da51b37afb02aaa2908ca31dfc4d8f94c4b
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-12 00:46:01 +0200

    Add orders components index for exports

---
+++ app/admin/page/orders/components/index.ts
@@ -0,0 +1,6 @@
+export { default as OrderStatsCards } from './OrderStatsCards';
+export { default as SearchFilterBar } from './SearchFilterBar';
+export { default as OrderCard } from './OrderCard';
+export { default as OrdersList } from './OrdersList';
+export { default as OrderDetailsModal } from './OrderDetailsModal';
+export { default as OrdersStyles } from './OrdersStyles';

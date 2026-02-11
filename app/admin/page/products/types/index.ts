fc14aff7523a0a2257d07a09b2d8e90413528293
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-12 01:05:05 +0200

    Add products types definition

---
+++ app/admin/page/products/types/index.ts
@@ -0,0 +1,15 @@
+export interface Product {
+  _id: string;
+  name: string;
+  nameAr: string;
+  mainImage: string;
+  basePrice: number;
+  finalPrice: number;
+  stock: number;
+  category: string;
+  brand: string;
+  averageRating: number;
+  totalReviews: number;
+  availability: string;
+  createdAt: string;
+}
